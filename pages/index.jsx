c2bd59c57600e9d1cfff3770cd24dce5a804d176
Aldimhr <dirikuin16@gmail.com>
2022-09-22 22:21:40 +0700

    feat(mobile): add producs and news letter section

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,23 @@
 import Image from 'next/image';
 import Header from '../components/Header';
+
 import playIcon from '../public/static/images/play-icon.svg';
 import searchIcon from '../public/static/images/search-icon.svg';
 import workIcon from '../public/static/images/work-icon.svg';
+import leftArrowIcon from '../public/static/images/leftArrow-icon.svg';
+import rightArrowIcon from '../public/static/images/rightArrow-icon.svg';
+import envelopeIcon from '../public/static/images/envelope-icon.svg';
+
 import stripeLogo from '../public/static/images/stripe-logo.png';
 import upworkLogo from '../public/static/images/upwork-logo.png';
 import gustoLogo from '../public/static/images/gusto-logo.png';
 import treehouseLogo from '../public/static/images/treehouse-logo.png';
+
+import product1 from '../public/static/images/product-1.png';
+import product2 from '../public/static/images/product-2.png';
+import product3 from '../public/static/images/product-3.png';
+import product4 from '../public/static/images/product-4.png';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
@@ -151,6 +162,70 @@ export default function Home() {
           </ul>
         </div>
       </section>
+
+      {/* products */}
+      <section className="py-8 bg-[#2F241F]">
+        <div className="px-4 flex justify-between items-center">
+          <div className="text-[#E5F0B6] ">
+            <h2 className="text-sm">Categories</h2>
+            <p className="font-bold text-lg">This month’s best seller</p>
+          </div>
+
+          <button className="text-xs px-4 py-3 bg-[#E5F0B6] rounded-2xl">SEE MORE</button>
+        </div>
+
+        <div className="relative mt-6 overflow-hidden">
+          {/* button */}
+          <div className="flex justify-between z-10 absolute top-1/2 left-0 right-0">
+            <div className="w-12 h-12 flex justify-center items-center rounded-full bg-[#E5F0B6]">
+              <Image src={leftArrowIcon} alt="left arrow" />
+            </div>
+
+            <div className="w-12 h-12 flex justify-center items-center rounded-full bg-[#E5F0B6]">
+              <Image src={rightArrowIcon} alt="right arrow" />
+            </div>
+          </div>
+
+          {/* product list */}
+          <div className="h-[256px] relative">
+            <ul className="flex gap-4 w-[300%]">
+              <li>
+                <Image src={product1} alt="product 1" />
+              </li>
+              <li>
+                <Image src={product2} alt="product 2" />
+              </li>
+              <li>
+                <Image src={product3} alt="product 3" />
+              </li>
+              <li>
+                <Image src={product4} alt="product 4" />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+
+      {/* news letter */}
+      <section className="py-8 px-4 bg-[#FCFAFA]">
+        <h2 className="text-sm text-center">LIMITED DEALS</h2>
+        <p className="font-bold text-lg text-center mt-1">
+          Become a member and get 10% off of your first purchase
+        </p>
+
+        <div className="flex mt-6">
+          <input
+            type="text"
+            placeholder="Enter your email here"
+            className="p-4 focus:outline-none rounded-l-xl"
+          />
+          <div className="bg-[#E5F0B6] rounded-r-xl p-4">
+            <Image src={envelopeIcon} alt="envelope icon" />
+          </div>
+        </div>
+      </section>
+
+      <Footer />
     </div>
   );
 }
