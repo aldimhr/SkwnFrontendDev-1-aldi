fe727b1d0ce3f598e6736bfc0b76613791225386
Aldimhr <dirikuin16@gmail.com>
2022-09-23 11:26:47 +0700

    fix: responsive input text

--- sections/home/NewsLetter.jsx
+++ sections/home/NewsLetter.jsx
@@ -8,7 +8,7 @@ export default function NewsLetter() {
       <p className="font-bold text-lg text-center mt-1">Become a member and get 10% off of your first purchase</p>
 
       <div className="flex mt-6">
-        <input type="text" placeholder="Enter your email here" className="p-4 focus:outline-none rounded-l-xl" />
+        <input type="text" placeholder="Enter your email here" className="p-4 w-full focus:outline-none rounded-l-xl" />
         <div className="bg-[#E5F0B6] rounded-r-xl p-4">
           <Image src={envelopeIcon} alt="envelope icon" />
         </div>
