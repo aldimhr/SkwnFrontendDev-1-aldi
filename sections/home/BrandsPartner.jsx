08daf7666cf0052aef5a56343cee9fc002b69852
Aldimhr <dirikuin16@gmail.com>
2022-09-23 12:47:03 +0700

    feat(responsive): brands partner section

--- sections/home/BrandsPartner.jsx
+++ sections/home/BrandsPartner.jsx
@@ -6,12 +6,12 @@ import treehouseLogo from '../../public/static/images/treehouse-logo.png';
 
 export default function BrandsPartner() {
   return (
-    <section className="py-10 px-6">
-      <h2 className="text-center text-sm">
-        <span className="block font-bold text-2xl ">25+</span> PARTNERED BRANDS
+    <section className="lg:px-[142px] py-10 px-6 lg:flex lg:items-center lg:gap-[150px]">
+      <h2 className="text-center text-sm lg:text-2xl">
+        <span className="block font-bold text-2xl lg:text-8xl">25+</span> PARTNERED BRANDS
       </h2>
 
-      <ul className="flex justify-between mt-6">
+      <ul className="flex justify-between mt-6 gap-8 lg:gap-[89px]">
         <li>
           <Image src={gustoLogo} alt="gusto logo" />
         </li>
