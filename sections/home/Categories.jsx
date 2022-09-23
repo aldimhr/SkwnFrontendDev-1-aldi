c2dcdf1c7b86ce74e04daadd2e16cc9bea7c638f
Aldimhr <dirikuin16@gmail.com>
2022-09-23 11:25:09 +0700

    feat(mobile): add categories desc toggle animation

--- sections/home/Categories.jsx
+++ sections/home/Categories.jsx
@@ -1,22 +1,49 @@
+import { useState } from 'react';
+
 export default function Categories() {
+  const [activeCategory, setActiveCategory] = useState('living-room');
+  const handleChange = (category) => {
+    setActiveCategory(category);
+  };
+
   return (
     <section className="px-4 py-6">
       <div className="px-[19px]">
         <h2 className="text-sm text-center">Categories</h2>
         <p className="text-center font-bold text-lg">Furniture Sets Recommendations</p>
-        <ul>
-          <li className="bg-[#FCFAFA] px-4 py-5">
-            <p className="font-bold text-lg">Bedroom</p>
+        <ul className="mt-6">
+          <li className={`px-4 py-5 ${activeCategory === 'bedroom' ? 'bg-[#E5F0B6]' : 'bg-[#FCFAFA]'}`}>
+            <p onClick={() => handleChange('bedroom')} className="font-bold text-lg">
+              Bedroom
+            </p>
+            <p className={activeCategory === 'bedroom' ? 'mt-3' : 'hidden'}>
+              Enjoy a great living room aesthetics with your family Designs created for increased comfortability
+            </p>
           </li>
-          <li className="bg-[#E5F0B6] px-4 py-5">
-            <p className="font-bold text-lg">Living Room</p>
-            <p className="mt-3">Enjoy a great living room aesthetics with your family Designs created for increased comfortability</p>
+          <li className={`px-4 py-5 ${activeCategory === 'living-room' ? 'bg-[#E5F0B6]' : 'bg-[#FCFAFA]'}`}>
+            <p onClick={() => handleChange('living-room')} className="font-bold text-lg">
+              Living Room
+            </p>
+
+            <p className={activeCategory === 'living-room' ? 'mt-3' : 'hidden'}>
+              Enjoy a great living room aesthetics with your family Designs created for increased comfortability
+            </p>
           </li>
-          <li className="bg-[#FCFAFA] px-4 py-5">
-            <p className="font-bold text-lg">Home Office</p>
+          <li className={`px-4 py-5 ${activeCategory === 'home-office' ? 'bg-[#E5F0B6]' : 'bg-[#FCFAFA]'}`}>
+            <p onClick={() => handleChange('home-office')} className="font-bold text-lg">
+              Home Office
+            </p>
+            <p className={activeCategory === 'home-office' ? 'mt-3' : 'hidden'}>
+              Enjoy a great living room aesthetics with your family Designs created for increased comfortability
+            </p>
           </li>
-          <li className="bg-[#FCFAFA] px-4 py-5">
-            <p className="font-bold text-lg">Gaming Room</p>
+          <li className={`px-4 py-5 ${activeCategory === 'gaming-room' ? 'bg-[#E5F0B6]' : 'bg-[#FCFAFA]'}`}>
+            <p onClick={() => handleChange('gaming-room')} className="font-bold text-lg">
+              Gaming Room
+            </p>
+            <p className={activeCategory === 'gaming-room' ? 'mt-3' : 'hidden'}>
+              Enjoy a great living room aesthetics with your family Designs created for increased comfortability
+            </p>
           </li>
         </ul>
       </div>
