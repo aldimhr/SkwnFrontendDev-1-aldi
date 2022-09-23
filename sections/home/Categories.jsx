83a596e0534458920d7fb5400daa1787b1e00d51
Aldimhr <dirikuin16@gmail.com>
2022-09-23 12:58:03 +0700

    feat(responsive): categories section

--- sections/home/Categories.jsx
+++ sections/home/Categories.jsx
@@ -1,4 +1,8 @@
+import Image from 'next/image';
 import { useState } from 'react';
+import category1 from '../../public/static/images/categories-1.png';
+import category2 from '../../public/static/images/categories-2.png';
+import category3 from '../../public/static/images/categories-3.png';
 
 export default function Categories() {
   const [activeCategory, setActiveCategory] = useState('living-room');
@@ -7,11 +11,17 @@ export default function Categories() {
   };
 
   return (
-    <section className="px-4 py-6">
-      <div className="px-[19px]">
-        <h2 className="text-sm text-center">Categories</h2>
-        <p className="text-center font-bold text-lg">Furniture Sets Recommendations</p>
-        <ul className="mt-6">
+    <section className="px-4 lg:px-0 py-6 lg:py-20 lg:flex lg:gap-12">
+      <div className="lg:flex hidden gap-6">
+        <Image src={category1} alt="category 1" />
+        <Image src={category2} alt="category 2" />
+        <Image src={category3} alt="category 3" />
+      </div>
+
+      <div className="px-[19px] lg:w-[523px] ">
+        <h2 className="text-sm text-center lg:text-left lg:text-2xl">Categories</h2>
+        <p className="text-center font-bold lg:mt-8 text-lg lg:text-left lg:text-5xl">Furniture Sets Recommendations</p>
+        <ul className="mt-6 lg:mt-8">
           <li className={`px-4 py-5 ${activeCategory === 'bedroom' ? 'bg-[#E5F0B6]' : 'bg-[#FCFAFA]'}`}>
             <p onClick={() => handleChange('bedroom')} className="font-bold text-lg">
               Bedroom
