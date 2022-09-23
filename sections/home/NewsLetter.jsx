7fae40d43dc1525fc81dfde9c0d9f006fd9eebff
Aldimhr <dirikuin16@gmail.com>
2022-09-23 13:12:24 +0700

    feat(responsive): news letter section

--- sections/home/NewsLetter.jsx
+++ sections/home/NewsLetter.jsx
@@ -3,12 +3,14 @@ import envelopeIcon from '../../public/static/images/envelope-icon.svg';
 
 export default function NewsLetter() {
   return (
-    <section className="py-8 px-4 bg-[#FCFAFA]">
-      <h2 className="text-sm text-center">LIMITED DEALS</h2>
-      <p className="font-bold text-lg text-center mt-1">Become a member and get 10% off of your first purchase</p>
+    <section className="py-8 px-4 lg:px-[142px] bg-[#FCFAFA]">
+      <h2 className="text-sm text-center lg:text-left lg:text-2xl">LIMITED DEALS</h2>
+      <p className="font-bold text-lg text-center mt-1 lg:text-left lg:text-[64px] lg:mt-[42px] lg:leading-none">
+        Become a member and get 10% off of your first purchase
+      </p>
 
-      <div className="flex mt-6">
-        <input type="text" placeholder="Enter your email here" className="p-4 w-full focus:outline-none rounded-l-xl" />
+      <div className="flex mt-6 lg:mt-[42px]">
+        <input type="text" placeholder="Enter your email here" className="p-4 lg:w-auto  w-full focus:outline-none rounded-l-xl" />
         <div className="bg-[#E5F0B6] rounded-r-xl p-4">
           <Image src={envelopeIcon} alt="envelope icon" />
         </div>
